50abf390d02e591b4b792621e694df85da8f2258
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-02-25 19:34:24 +0100

    .

--- tests/graphql-testing-server/src/main.ts
+++ tests/graphql-testing-server/src/main.ts
@@ -1,9 +1,9 @@
 import {StarWarsSchema} from "./StarWarsSchema";
 
-const express = require("express-graphql");
+const express = require("express");
 const graphqlHTTP = require("express-graphql");
-
 const app = express();
+
 app.use(process.env.npm_package_config_graphql, graphqlHTTP({
 	graphiql: true,
 	schema:  new StarWarsSchema(),
