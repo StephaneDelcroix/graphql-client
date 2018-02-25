ff4459c9307d8254cd680075b9a5091b3a36c14f
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-02-25 19:44:56 +0100

    with ES5

--- tests/graphql-testing-server/src/main.ts
+++ tests/graphql-testing-server/src/main.ts
@@ -1,10 +1,9 @@
+import * as express from "express";
+import * as graphqlHTTP from "express-graphql";
 import {StarWarsSchema} from "./StarWarsSchema";
-
-const express = require("express");
-const graphqlHTTP = require("express-graphql");
 const app = express();
 
-app.use(process.env.npm_package_config_graphql, graphqlHTTP({
+app.use(process.env.npm_package_config_graphql as string, graphqlHTTP({
 	graphiql: true,
 	schema:  new StarWarsSchema(),
 }));
